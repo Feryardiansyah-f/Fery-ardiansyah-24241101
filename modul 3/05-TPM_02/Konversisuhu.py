c28f33c62d52731433840199de6b0acd7f6135ca
Fery ardiansyah <feryardiansyah4h@gmail.com>
2024-11-05 22:16:22 +0800

    Add files via upload

---
+++ modul 3/05-TPM_02/Konversisuhu.py
@@ -0,0 +1,10 @@
+# input suhu dalam Fahrenheit
+fahrenheit= float(input("masukan suhu dalam fahrenheit:"))
+
+#konversi ke celcius
+celsius = (5/9) * (fahrenheit - 32)
+
+# Tampilkan hasilnya
+print(f"{fahrenheit} Fahrenheit sama dengan {celsius} Celsius")
+
+ 
